db5e932de555d05b055589228dd7d777bedc6121
Justin Corbin <jcorbin2290@gmail.com>
2019-12-06 12:12:55 -0700

    couldnt get fucntionality

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+// document.getElementById("happy").addEventListner("click", function(){
+//   document.getElementById("h2").innerHTML = "hello world";
+//   console.log("hello");
+// });
 
+
+document.getElementByClassName("happy").addEventListener("click", function(){
+  document.getElementByClassName("message").innerHTML = "Hello World";
+});
